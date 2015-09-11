ac03d1752c665bf7bb6a765d3335fe59a0527de4
kisieldk <dkisielewski@gmail.com>
2015-09-11 22:42:55 +0200

    sdf

---
+++ waluta/sw.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="sw.asmx.cs" Class="waluta.sw" %>
---
+++ waluta/sw.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace waluta
+{
+    /// <summary>
+    /// Summary description for sw
+    /// </summary>
+    [WebService(Namespace = "http://webserv-1.apphb.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class sw : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ZwrocPLN(string GBP)
+        {
+            double _GBP = Convert.ToDouble(GBP);
+            double kurs = 0.0000;
+            double PLN = 0.0000;
+            DataSet ds = new DataSet();
+
+            ds.ReadXml("http://www.nbp.pl/kursy/xml/LastA.xml");
+            foreach (DataRow item in ds.Tables[1].Rows)
+            {
+                if (item["kod_waluty"].ToString() == "GBP")
+                {
+                    kurs = Convert.ToDouble(item["kurs_sredni"].ToString());
+                }
+
+            }
+            PLN = _GBP * kurs * 100;
+
+            return PLN.ToString();
+        }
+    }
+}
