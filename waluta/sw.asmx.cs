e51f2e263e266f50a519b74bd142a9da22c87a36
kisieldk <dkisielewski@gmail.com>
2015-09-13 15:22:59 +0200

    ghfhgf

--- waluta/sw.asmx.cs
+++ waluta/sw.asmx.cs
@@ -17,27 +17,37 @@ namespace waluta
     // [System.Web.Script.Services.ScriptService]
     public class sw : System.Web.Services.WebService
     {
-
+        private string url = "http://www.nbp.pl/kursy/xml/LastA.xml";
         [WebMethod]
         public double ZwrocPLN(double GBP)
         {
-            double _GBP = GBP;
-            double kurs = 0.0000;
-            double PLN = 0.00;
+          
+            double kurs = 0;
+            double PLN = 0;
             DataSet ds = new DataSet();
 
             ds.ReadXml("http://www.nbp.pl/kursy/xml/LastA.xml");
-            foreach (DataRow item in ds.Tables[1].Rows)
-            {
-                if (item["kod_waluty"].ToString() == "GBP")
-                {
-                    kurs = Convert.ToDouble(item["kurs_sredni"].ToString());
-                }
-
-            }
-            PLN = _GBP * kurs;
+            var s = ds.Tables[1].Select("kod_waluty = 'GBP'");
+            kurs = Double.Parse(s[0]["kurs_sredni"].ToString());
+            PLN = GBP * kurs;
             PLN = Math.Round(PLN,2);
             return PLN;
+        }
+
+        [WebMethod]
+        public double ZamienWalute(double kwota, string waluta)
+        {
+        
+            double kurs = 0;
+            double nowaKwota = 0;
+            DataSet ds = new DataSet();
+
+            ds.ReadXml(url);
+            var s = ds.Tables[1].Select(string.Format("kod_waluty = '{0}'",waluta));
+            kurs = Double.Parse(s[0]["kurs_sredni"].ToString());
+
+            nowaKwota = kwota / kurs;
+            return nowaKwota;
         }
     }
 }
